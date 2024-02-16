224a46fa026faa319b0a4c34a0d7194eaa70bb79
jungle379 <jungle379@gmail.com>
2024-02-16 22:07:44 +0900

    add_pages

---
+++ app/components/client.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useState } from "react";
+
+export const ClientComponent = () => {
+  const [client, setClient] = useState();
+  return <div>client</div>;
+};
---
+++ app/components/server.tsx
@@ -0,0 +1,3 @@
+export const ServerComponent = () => {
+  return <div>server</div>;
+};
