7d78e3d9c1ffce958bd76c5a79bddb9cbfde3cb6
jungle379 <jungle379@gmail.com>
2024-02-25 09:55:19 +0900

    URL追加

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 export const Footer = () => {
+  const INSTA_URL = process.env.LOCAL_INSTA;
   return (
     <>
       <div className="flex justify-center">
@@ -8,7 +9,7 @@ export const Footer = () => {
           <Link href="/">X</Link>
         </div>
         <div className="px-5">
-          <Link href="/">インスタ</Link>
+          <Link href={`${INSTA_URL}`}>インスタ</Link>
         </div>
         <div className="px-5">...</div>
         <div className="px-5">...</div>
