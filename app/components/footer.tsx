31793c6822f7bd2399d6322a58c6bde924e0698d
jungle379 <jungle379@gmail.com>
2024-02-27 06:11:38 +0900

    イメージ、アイコン追加

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,18 +1,16 @@
 import Link from "next/link";
+import { FaInstagram } from "react-icons/fa6";
 
 export const Footer = () => {
   const INSTA_URL = process.env.REACT_APP_INSTA;
   return (
     <>
-      <div className="flex justify-center">
-        <div className="px-5">
-          <Link href="/">X</Link>
+      <div className="flex justify-center h-[100px]">
+        <div className="py-5 px-5">
+          <Link href={`${INSTA_URL}`}>
+            <FaInstagram size={50} />
+          </Link>
         </div>
-        <div className="px-5">
-          <Link href={`${INSTA_URL}`}>インスタ</Link>
-        </div>
-        <div className="px-5">...</div>
-        <div className="px-5">...</div>
       </div>
     </>
   );
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,13 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export const Header = () => {
   return (
     <>
-      <div className="flex justify-between h-[200px]">
-        <div className="pt-5">
-          <div>
-            <Link href="/">イメージ入れる</Link>
-          </div>
+      <div className="flex justify-between h-[100px]">
+        <div className="pt-2">
+          <Link href="/">
+            <Image src="/1.png" alt={"icon"} width={80} height={80} />
+          </Link>
         </div>
         <div className="px-10 pt-5">
           <div className="flex justify-between">
