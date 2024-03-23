581acad5e2e9e27728acca693c031bf5083f4be8
jungle379 <jungle379@gmail.com>
2024-03-23 20:25:56 +0900

    s

--- app/components/client.tsx
+++ app/components/client.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { PaperComp } from "./paper";
 import { TextGenerateEffectDemo } from "./text";
 
 export const ClientComponent = () => {
   return (
     <>
       <TextGenerateEffectDemo />
+      <PaperComp />
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="min-h-screen bg-blue-50">
+      <main className="min-h-screen">
         <ClientComponent />
         <ServerComponent />
       </main>
