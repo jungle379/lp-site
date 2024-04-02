3968a8b5f229ca3f2c01b824b26d7a8f5cbd92e3
jungle379 <jungle379@gmail.com>
2024-04-02 21:46:23 +0900

    syuusei

--- app/introduce/page.tsx
+++ app/introduce/page.tsx
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <>
       <main className="h-screen bg-green-300">
-        <div className="flex justify-center items-center text-center py-20 px-20">
+        <div className="flex justify-center items-center text-center pt-20 pb-5 px-20">
           <Image src="/2.png" alt={"icon"} width={180} height={180} />
+        </div>
+        <div className="flex justify-center text-lg items-center font-bold text-center py-5 px-20">
+          あむ
         </div>
       </main>
     </>
