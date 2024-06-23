2caea050f60dc3189c21f123480de97a64609cbd
jungle379 <jungle379@gmail.com>
2024-06-24 06:45:35 +0900

    change_image

--- app/introduce/page.tsx
+++ app/introduce/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
       <main className="min-h-screen bg-green-300">
-        <div className="flex justify-center items-center text-center pt-20 pb-5 px-20">
-          <Image src="/2.png" alt={"icon"} width={180} height={180} />
+        <div className="flex justify-center items-center text-center py-10 px-20">
+          <Image src="/2.jpg" alt={"icon"} width={300} height={300} />
         </div>
         <div className="flex justify-center text-lg items-center font-bold text-center py-5 px-20">
           あむ
