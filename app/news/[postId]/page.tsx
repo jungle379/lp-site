bded301cb36a1904f896f8aa241907786e23437c
jungle379 <jungle379@gmail.com>
2024-08-16 20:20:14 +0900

    a

--- app/news/[postId]/page.tsx
+++ app/news/[postId]/page.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { getDetail, getList } from "../../libs/client";
 import parse from "html-react-parser";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 export async function generateStaticParams() {
   const { contents } = await getList();
@@ -29,6 +30,9 @@ export default async function StaticDetailPage({
         <title>{post.title}</title>
       </Head>
       <div className="bg-green-100 min-h-screen h-auto">
+        <Link href="/" className="p-5 font-bold text-xl">
+          ← 戻る
+        </Link>
         <div className="p-5 font-bold text-xl">{post.title}</div>
         <div className="p-5">{parse(post.content)}</div>
       </div>
