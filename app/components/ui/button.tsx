485cdd6cb6cd4924eabc8b5ca86409c9db05d707
jungle379 <jungle379@gmail.com>
2024-11-23 14:23:56 +0900

    syuusei

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -5,7 +5,11 @@ import { Button } from "@mantine/core";
 export const RouteButton = () => {
   const router = useRouter();
   return (
-    <Button variant="filled" onClick={() => router.push("/")}>
+    <Button
+      className="my-10 bg-blue-500"
+      variant="filled"
+      onClick={() => router.push("/")}
+    >
       ホームに戻る
     </Button>
   );
