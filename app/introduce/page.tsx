32ae5831194a0c80811c7f33e2ba47987cc72fd2
jungle379 <jungle379@gmail.com>
2024-11-23 15:17:47 +0900

    修正

--- app/introduce/page.tsx
+++ app/introduce/page.tsx
@@ -10,7 +10,7 @@ export default function Introduce() {
       <div className="flex justify-center text-2xl items-center font-bold text-center py-5 px-20">
         あむ
       </div>
-      <div className="px-4 py-5">
+      <div className="flex justify-center text-center">
         <TextGenerateEffectDemo />
       </div>
     </>
