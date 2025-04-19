1e307b7b10d3297ef6f256736cff572ecef3b787
jungle379 <jungle379@gmail.com>
2025-04-19 17:30:15 +0900

    画像変更

--- app/components/client.tsx
+++ app/components/client.tsx
@@ -15,9 +15,8 @@ export const ClientComponent = () => {
             <CardContainer className="inter-var">
               <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border mx-12">
                 <CardItem translateZ="100" className="w-full mt-4">
-                  <Image
-                    src="/1.png"
-                    priority
+                  <img
+                    src="/cat.jpg"
                     height="1000"
                     width="1000"
                     className="md:h-120 md:w-90 object-cover rounded-xl group-hover/card:shadow-xl"
