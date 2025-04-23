e0e12e00e35fd288d14ab78e5a89dba3178a6d6a
jungle379 <jungle379@gmail.com>
2025-04-24 06:16:32 +0900

    修正

--- app/components/server.tsx
+++ app/components/server.tsx
@@ -7,14 +7,6 @@ import Loading from "./ui/loading";
 export default async function ServerComponent() {
   const { contents } = await getList();
 
-  if (!contents || contents.length === 0) {
-    return (
-      <h1 className="m-3 flex justify-center font-bold">
-        投稿の取得に失敗しました！
-      </h1>
-    );
-  }
-
   // 日付でソートして最新の5件を取得
   const latestPosts = [...contents]
     .sort(
@@ -25,25 +17,33 @@ export default async function ServerComponent() {
 
   return (
     <Suspense fallback={<Loading />}>
-      <div>
+      <>
         <div className="text-xl text-thin my-2 md:my-8 mx-8 md:mx-14">
           最新の投稿
         </div>
-        <div className="w-full md:hover:underline md:hidden">
-          <ul>
-            {latestPosts.map((post) => (
-              <li key={post.id}>
-                <div className="py-2 my-5 px-4 mx-8 font-bold text-xl bg-gray-100">
-                  <Link href={`news/${post.id}`}>{post.title}</Link>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="my-10 mx-8 md:mx-14">
-          <TableUI contents={latestPosts} />
-        </div>
-      </div>
+        {!contents || contents.length === 0 ? (
+          <div className="m-3 flex justify-center font-bold text-xl">
+            投稿の取得に失敗しました！
+          </div>
+        ) : (
+          <>
+            <div className="w-full md:hover:underline md:hidden">
+              <ul>
+                {latestPosts.map((post) => (
+                  <li key={post.id}>
+                    <div className="py-2 my-5 px-4 mx-8 font-bold text-xl bg-gray-100">
+                      <Link href={`news/${post.id}`}>{post.title}</Link>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="my-10 mx-8 md:mx-14">
+              <TableUI contents={latestPosts} />
+            </div>
+          </>
+        )}
+      </>
     </Suspense>
   );
 }
