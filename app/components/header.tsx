f29799ecef13e4714d86d64d2fc55f8f512b1228
jungle379 <jungle379@gmail.com>
2025-04-26 06:34:10 +0900

    header修正

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,39 +1,14 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 import { MenuComp } from "./menu";
 
 export const Header = () => {
-  const [isMobile, setIsMobile] = useState(true);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    handleResize(); // 初期化時に1度実行
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <>
       <div className="bg-pink-100 h-[100px]">
-        <div>
-          <div
-            className={`flex items-center justify-between pt-6 px-5 text-xl font-bold ${isMobile ? "hidden" : ""}`}
-          >
-            <Link href="/">あむのジュエリーボックス</Link>
-            <MenuComp />
-          </div>
-          <div
-            className={`items-center justify-between flex pt-9 pl-4 font-bold text-xs ${isMobile ? "" : "hidden"}`}
-          >
-            <Link href="/">あむのジュエリーボックス</Link>
-            <MenuComp />
-          </div>
+        <div className="flex items-center justify-between pt-9 md:pt-6 pl-4 md:px-5 text-xs md:text-xl font-bold">
+          <Link href="/">あむのジュエリーボックス</Link>
+          <MenuComp />
         </div>
       </div>
     </>
