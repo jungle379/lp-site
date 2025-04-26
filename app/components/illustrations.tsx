45c48dfba5bfd1515c6aba0e3e2827aa59cf0edd
jungle379 <jungle379@gmail.com>
2025-04-26 16:26:47 +0900

    syuusei

--- app/components/illustrations.tsx
+++ app/components/illustrations.tsx
@@ -6,26 +6,28 @@ import { getIllustrationList } from "../libs/client";
 export default async function Illustrations() {
   const { contents } = await getIllustrationList();
 
-  if (!contents || contents.length === 0) {
-    return <h1>投稿の取得に失敗しました！</h1>;
-  }
-
   return (
     <Suspense fallback={<Loading />}>
       <div>
-        <div className="flex justify-center text-xl text-thin underline my-2 md:my-8 mx-8 md:mx-14 hover:underline text-blue-500">
-          <ul>
-            {contents.map((post) => {
-              return (
-                <li key={post.id}>
-                  <div className="m-6 font-bold text-xl">
-                    <Link href={`/illustrations/${post.id}`}>ほかの作品</Link>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        {!contents || contents.length === 0 ? (
+          <div className="mt-4 mb-10 flex justify-center font-bold text-xl">
+            投稿の取得に失敗しました！
+          </div>
+        ) : (
+          <div className="flex justify-center text-xl text-thin underline my-2 md:my-8 mx-8 md:mx-14 hover:underline text-blue-500">
+            <ul>
+              {contents.map((post) => {
+                return (
+                  <li key={post.id}>
+                    <div className="m-6 font-bold text-xl">
+                      <Link href={`/illustrations/${post.id}`}>ほかの作品</Link>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
       </div>
     </Suspense>
   );
