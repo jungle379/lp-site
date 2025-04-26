81f5daada52be714b421ea845955d91a541a84b8
jungle379 <jungle379@gmail.com>
2025-04-26 16:43:13 +0900

    画像名称変更

--- app/introduce/page.tsx
+++ app/introduce/page.tsx
@@ -6,7 +6,7 @@ export default function Introduce() {
   return (
     <>
       <div className="flex justify-center items-center text-center py-10 px-20">
-        <Image src="/2.jpg" alt={"icon"} width={250} height={250} />
+        <Image src="/portrait.jpg" alt={"icon"} width={250} height={250} />
       </div>
       <div className="flex justify-center text-4xl items-center font-bold text-center py-5 px-20">
         あむ
