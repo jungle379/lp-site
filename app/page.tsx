d8c8ec3e1c7c8877c53e462a3190df38d8cef0fb
jungle379 <jungle379@gmail.com>
2025-05-13 16:10:39 +0900

    syuusei

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <>
       <Suspense fallback={<Loading />}>
-        <ClientComponent />
         <Illustrations />
         <ServerComponent />
       </Suspense>
