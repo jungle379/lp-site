8e4cf471b49a45d3061588a0afd7aa3e66a3cf3f
jungle379 <jungle379@gmail.com>
2025-05-13 16:20:49 +0900

    syuusei

--- app/components/illustrations.tsx
+++ app/components/illustrations.tsx
@@ -1,22 +1,10 @@
 import Link from "next/link";
 import { Suspense } from "react";
 import Loading from "./ui/loading";
-import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
-import { Grid } from "@mantine/core";
 import { getIllustrationList } from "../libs/client";
 
 export default async function Illustrations() {
   const { contents } = await getIllustrationList();
-  // 日付でソートして最新の5件を取得
-  const latestPosts = [...contents]
-    .sort(
-      (a, b) =>
-        new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
-    )
-    .slice(0, 1);
-  // const handleContextMenu = (evt: any) => {
-  //   evt.preventDefault();
-  // };
 
   return (
     <Suspense fallback={<Loading />}>
@@ -26,47 +14,19 @@ export default async function Illustrations() {
             現在、他のイラストの投稿はありません
           </div>
         ) : (
-          <>
-            <div>
-              <div className="text-xl text-thin mt-4 md:my-8 mx-8 md:mx-14 md:text-4xl md:font-bold">
-                作品
-              </div>
-              {latestPosts.map((post) => (
-                <Grid grow gutter="xs">
-                  <Grid.Col span={4}>
-                    <CardContainer className="inter-var">
-                      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border mx-12">
-                        <CardItem translateZ="100" className="w-full">
-                          <img
-                            key={post.id}
-                            height="1000"
-                            width="1000"
-                            className="md:h-120 md:w-90 object-cover rounded-xl group-hover/card:shadow-xl"
-                            alt="thumbnail"
-                          />
-                        </CardItem>
-                      </CardBody>
-                    </CardContainer>
-                  </Grid.Col>
-                </Grid>
-              ))}
-            </div>
-            <div className="flex justify-center text-xl text-thin underline my-2 md:my-8 mx-8 md:mx-14 hover:underline text-blue-500">
-              <ul>
-                {contents.map((post) => {
-                  return (
-                    <li key={post.id}>
-                      <div className="m-6 font-bold text-xl">
-                        <Link href={`/illustrations/${post.id}`}>
-                          ほかの作品
-                        </Link>
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </>
+          <div className="flex justify-center text-xl text-thin underline my-2 md:my-8 mx-8 md:mx-14 hover:underline text-blue-500">
+            <ul>
+              {contents.map((post) => {
+                return (
+                  <li key={post.id}>
+                    <div className="m-6 font-bold text-xl">
+                      <Link href={`/illustrations/${post.id}`}>ほかの作品</Link>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         )}
       </div>
     </Suspense>
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <>
       <Suspense fallback={<Loading />}>
+        <ClientComponent />
         <Illustrations />
         <ServerComponent />
       </Suspense>
