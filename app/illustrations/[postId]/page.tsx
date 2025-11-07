1a490db62576bdc3b1b8e1955301b4a25ddf748d
jungle379 <jungle379@gmail.com>
2025-11-08 08:08:01 +0900

    syuusei

--- app/illustrations/[postId]/page.tsx
+++ app/illustrations/[postId]/page.tsx
@@ -9,7 +9,7 @@ export default async function Illustrations() {
 
   return (
     <>
-      <Box className="max-w-5xl mx-auto p-4">
+      <Box className="max-w-5xl mx-auto m-4">
         <h2 className="text-center text-2xl font-bold my-6">作品一覧</h2>
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {contents.map((post) => (
